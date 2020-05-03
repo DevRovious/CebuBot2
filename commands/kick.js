e369ccf1d6290465525dbef02f978ce970d056e8
Jelte Cost <j.cost@cadicto.nl>
2020-05-03 23:03:12 +0200

    3-5-2020 // 23;03
    Published all Commands

---
+++ commands/kick.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+
+    return message.channel.send("> **This funtion is under development!**");
+
+}
+
+
+module.exports.help = {
+    name: "kick"
+}
