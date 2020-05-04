d8920f49dc972fd3f430273bc4b8650373d61183
Jelte Cost <j.cost@cadicto.nl>
2020-05-04 17:24:54 +0200

    Added ticket

--- commands/Help.js
+++ commands/Help.js
@@ -2,9 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    message.channel.send("Test")
-    await
-    message.channel.send("> **Following commands are:** \n > \n > - .help \n > - .about \n > - .preflix \n > - .report \n > - .support \n > - .ticket \n > - .updates \n >  \n > **Following staff commands are:** \n >  \n > - .announce \n > - .kick \n > - .ban \n > \n > **More commands are comming soon!** ");
+    return message.channel.send("> **Following commands are:** \n > \n > - .help \n > - .about \n > - .preflix \n > - .report \n > - .support \n > - .ticket \n > - .updates \n >  \n > **Following staff commands are:** \n >  \n > - .announce \n > - .kick \n > - .ban \n > \n > **More commands are comming soon!** ");
 }
 
 
--- commands/tickets.js
+++ commands/tickets.js
@@ -1,13 +1,39 @@
 const discord = require("discord.js");
 
-module.exports.run = async(bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
+    // Id of the tickets category.
+    const categoryId = "497424761765232650";
 
+    // If message is in a ticket channel
+    if (message.channel.parentID == categoryId) {
 
-    return message.channel.send("> **This funtion is under development!**");
+        message.channel.delete();
+
+    } else { // If not
+
+        message.channel.send("Gelieve dit commando in een ticket kanaal uit te voeren.");
+
+    }
+
+    // Create embed.
+    var embedCreateTicket = new discord.RichEmbed()
+        .setTitle("Hoi, " + message.channel.name)
+        .setDescription("Je ticket is gemarkeerd als **compleet**. Wil je een nieuwe maken doe dan ?ticket")
+        .setFooter("Ticket gesloten");
+
+    // Find channel for the log.
+    var ticketChannel = message.guild.channels.find(`name`, "logs");
+    if (!ticketChannel) return message.guild.send("Kan het kanaal niet vinden");
+
+    // Send embed.
+    ticketChannel.send(embedCreateTicket);
 
 }
 
+module.exports.conf = {
+    aliases: []
+};
 
 module.exports.help = {
-    name: "ticket"
+    name: "close"
 }
