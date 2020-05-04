81bf7c7a44d4e3f8141b2db0d3658517f2aecad3
Jelte Cost <j.cost@cadicto.nl>
2020-05-04 17:20:05 +0200

    Update Help.js

--- commands/Help.js
+++ commands/Help.js
@@ -4,8 +4,8 @@ module.exports.run = async(bot, message, args) => {
 
 
     return message.channel.send("> **Following commands are:** \n > \n > - .help \n > - .about \n > - .preflix \n > - .report \n > - .support \n > - .ticket \n > - .updates \n >  \n > **Following staff commands are:** \n >  \n > - .announce \n > - .kick \n > - .ban \n > \n > **More commands are comming soon!** ");
-    
-
+    await
+    message.channel.send("Test")
 }
 
 
