334fa28ba1cb1d7b1d4e95416db5c7c6c2988d44
Jelte Cost <j.cost@cadicto.nl>
2020-05-04 17:21:34 +0200

    Update Help.js

--- commands/Help.js
+++ commands/Help.js
@@ -2,10 +2,9 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-
-    return message.channel.send("> **Following commands are:** \n > \n > - .help \n > - .about \n > - .preflix \n > - .report \n > - .support \n > - .ticket \n > - .updates \n >  \n > **Following staff commands are:** \n >  \n > - .announce \n > - .kick \n > - .ban \n > \n > **More commands are comming soon!** ");
-    await
     message.channel.send("Test")
+    await
+    message.channel.send("> **Following commands are:** \n > \n > - .help \n > - .about \n > - .preflix \n > - .report \n > - .support \n > - .ticket \n > - .updates \n >  \n > **Following staff commands are:** \n >  \n > - .announce \n > - .kick \n > - .ban \n > \n > **More commands are comming soon!** ");
 }
 
 
