955d5e17399f7ad659ac02c4343cfb2cb7d08f2b
Jelte Cost <j.cost@cadicto.nl>
2020-05-05 21:02:39 +0200

    ad

--- commands/Make_Bot_Offline.js
+++ commands/Make_Bot_Offline.js
@@ -3,6 +3,8 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
     console.log(`${client.user.username} is offline.`);
+    const newLocal = message.channel.send("Bot is offline");
+    message.channel.send("Bot is offline")
 }
 
 
--- commands/Make_Bot_Online.js
+++ commands/Make_Bot_Online.js
@@ -3,7 +3,8 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
     console.log(`${client.user.username} is online.`);
-
+    const newLocal = message.channel.send("Bot is online");
+    message.channel.send("Bot is online")
 }
 
 
