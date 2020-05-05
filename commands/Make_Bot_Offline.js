d5f3381b2b2de92f9de80147feb6d2ffd7c575d1
Jelte Cost <j.cost@cadicto.nl>
2020-05-05 21:01:25 +0200

    Bot online and ofline updated test

--- commands/Make_Bot_Offline.js
+++ commands/Make_Bot_Offline.js
@@ -2,14 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    client.on("ready", async () => {
-
-        console.log(`${client.user.username} is offline.`);
-    
-        client.user.setActivity("I'm getting developed!", { type: "WATCHING" });
-    
-    });
-
+    console.log(`${client.user.username} is offline.`);
 }
 
 
--- commands/Make_Bot_Online.js
+++ commands/Make_Bot_Online.js
@@ -2,13 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    client.on("ready", async () => {
-
-        console.log(`${client.user.username} is online.`);
-    
-        client.user.setActivity("Watching Cebu Pacific Roblox Discord Server!", { type: "PLAYING" });
-    
-    });
+    console.log(`${client.user.username} is online.`);
 
 }
 
