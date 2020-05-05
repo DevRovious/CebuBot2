2e9a0c65eac202ee3f3035fe71471d4878170874
Jelte Cost <j.cost@cadicto.nl>
2020-05-05 22:36:37 +0200

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -2,9 +2,9 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    message.delete()
-    message.channel.send("> **This funtion is under development!**")
-    await message.delete()
+    message.delete();
+    message.channel.send("> **This funtion is under development!**");
+    await message.delete();
     return;
 
 }
