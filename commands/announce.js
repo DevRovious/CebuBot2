0aca67cc161b63ce61e7d774d65353cb41f95dff
Jelte Cost <j.cost@cadicto.nl>
2020-05-06 20:06:12 +0200

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -10,7 +10,7 @@ module.exports.run = async(bot, message, args) => {
     var announceEmbed = new discord.MessageEmbed()
         .setTitle(`📢  ${message.guild.name} | **Flight Announcement**  📢`)
         .setFooter(`Sendend by ${message.author.tag}`)
-        .setThumbnail(icon)
+        .setThumbnail(boticon)
         .setColor("00ff26")
         .setTimestamp()
         .setDescription(`**Flight: ** *number* \n **Plane: **  *planetype* \n **Host:** *${message.author.tag}* \n \n **Times:** \n *Serveropentime* \n *serverclosetime* \n *boardingtime* \n \n **Link: ** \n *link*`);
