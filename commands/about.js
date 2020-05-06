b0cef0b41d57ccaa08c26c0b97cab3d962a24687
Jelte Cost <j.cost@cadicto.nl>
2020-05-06 20:31:42 +0200

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -10,7 +10,7 @@ module.exports.run = async(bot, message, args) => {
         .setTitle(`You used the command: .about`)
         //.setThumbnail(boticon)
         .setColor("00ff26")
-        .setDescription(`**About:** \n\n Info: \n > *Unknown*. \n\n Time created: \n *- 3-5-2020 18:55 CET*. \n\n reator: \n *- DevRovious*`);
+        .setDescription(`**About:** \n\n Info: \n Welcome to the *About* from ${bot.name}. \n\n Time created: \n *- 3-5-2020 18:55 CET*. \n\n reator: \n *- DevRovious*`);
         
  
     // Send the embed to the current channel
