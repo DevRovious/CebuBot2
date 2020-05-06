61465c2d3e2d59aa738a4825069598e62adf6564
Jelte Cost <j.cost@cadicto.nl>
2020-05-06 20:18:02 +0200

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -2,10 +2,20 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    message.delete();
-    return message.channel.send("> **About:** \n > \n > Info: \n > *Unknown*. \n > \n > Time created: \n > *- 3-5-2020 18:55 CET*. \n > \n > Creator: \n > *- DevRovious*");
     
-
+    message.delete();
+    var icon = message.guild.iconURL;
+    // Make an embed
+    var announceEmbed = new discord.MessageEmbed()
+        .setTitle(`.aboout`)
+        //.setThumbnail(boticon)
+        .setColor("00ff26")
+        .setDescription(`> **About:** \n > \n > Info: \n > *Unknown*. \n > \n > Time created: \n > *- 3-5-2020 18:55 CET*. \n > \n > Creator: \n > *- DevRovious*`);
+        
+ 
+    // Send the embed to the current channel
+    var embedSend = await message.channel.send(announceEmbed);
+    return;
 }
 
 
