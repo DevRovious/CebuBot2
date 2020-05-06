7b58512633ab1ad3938ca6152ea9270142260f5d
DevRovious <64751936+DevRovious@users.noreply.github.com>
2020-05-06 14:26:17 +0200

    Update SpecialCode.js

--- commands/SpecialCode.js
+++ commands/SpecialCode.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
 
     message.delete();
-    return message.channel.send("> **https://twitter.com/rbxcebupacific/status/1257961677804322816**");
+    return message.channel.send("> **New Twitter post!** \n https://twitter.com/rbxcebupacific/status/1257961677804322816");
     
 
 }
