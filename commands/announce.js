737919afc80866afb92eff1e9d9f508af5f3fe74
Jelte Cost <j.cost@cadicto.nl>
2020-05-06 19:07:32 +0200

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -2,8 +2,21 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
+
+
     message.delete();
-    return message.channel.send("> **This funtion is under development!**")
+
+    var icon = message.guild.iconURL;
+
+    var serverEmbed = new discord.RichEmbed()
+    .setDescription("Test")
+    .setColor("#00ff26")
+    .setThumbnail(icon)
+    .addField("From * to *")
+    .addField("Time *");
+
+
+    return message.channel.send(serverEmbed)
     
     return;
 }
