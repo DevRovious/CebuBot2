61d7c9adbefdcbe9151c16d39a6b1e7e388faa52
Jelte Cost <j.cost@cadicto.nl>
2020-05-08 20:42:40 +0200

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -8,7 +8,7 @@ module.exports.run = async(client, message, args) => {
     var icon = message.guild.iconURL;
     // Make an embed
     var announceEmbed = new discord.MessageEmbed()
-        .setTitle(`📢  ${message.guild.name} | **Flight Announcement:**  📢`)
+        .setTitle(`:cebulogo: Cebu Pacific Roblox | Flight`)
         .setFooter(`Sendend by ${message.author.tag}`)
         //.setThumbnail(boticon)
         .setColor("00ff26")
