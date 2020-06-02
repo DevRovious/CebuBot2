6067e1d8bf643a123b490561bc8736f015114679
Jelte Cost <j.cost@cadicto.nl>
2020-06-02 19:11:40 +0200

    d

--- commands/applications.js
+++ commands/applications.js
@@ -2,22 +2,20 @@ const discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
 
-    
     var icon = message.guild.iconURL;
     // Make an embed
     var announceEmbed = new discord.MessageEmbed()
-        .setTitle(`Applications Hub:`)
+        .setTitle(`Applications:`)
         //.setThumbnail(boticon)
         .setColor("1a4ebd")
-        .setDescription(`**https://www.roblox.com/games/5126107873/British-Airways-Low-Ranks-Applications`);
+        .setDescription(`https://www.roblox.com/games/5126107873/British-Airways-Low-Ranks-Applications`);
         
- 
-    // Send the embed to the current channel
-    var embedSend = await message.channel.send(announceEmbed);
-    return;
+        var embedSend = await message.channel.send(announceEmbed);
+        return;
+
 }
 
 
 module.exports.help = {
-    name: "application"
+    name: "commands"
 }
