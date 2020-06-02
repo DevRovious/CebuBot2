8a36ed3bf2d28ef7d0aaab447b6218d044c75c23
Jelte Cost <j.cost@cadicto.nl>
2020-06-02 19:28:42 +0200

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,17 +1,17 @@
 const discord = require("discord.js");
 
-module.exports.run = async(client, message, args) => {
+module.exports.run = async(client, message, args, boticon) => {
 
     message.delete();
     var icon = message.guild.iconURL;
     // Make an embed
     var announceEmbed = new discord.MessageEmbed()
-        .setTitle(`Britisch Airways Roblox | Announcement`)
+        .setTitle(`Britisch Airways Roblox | Developments`)
         .setFooter(`Sendend by ${message.author.tag}`)
-        //.setThumbnail(boticon)
+        .setThumbnail(boticon)
         .setColor("1a4ebd")
         .setTimestamp()
-        .setDescription(`**UPDATES:** \n \n - Self Check In \n - New In Flight Entertrainment \n - More coming soon!`);
+        .setDescription(`**UPDATES:** \n \n - Self Check In \n - New In Flight Entertrainment \n - More coming soon! \n`);
         
  
     // Send the embed to the current channel
@@ -21,5 +21,5 @@ module.exports.run = async(client, message, args) => {
 
 
 module.exports.help = {
-    name: "announce"
+    name: "Dev"
 }
