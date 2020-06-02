07049a9528355a22c29c365d15da3b307f5185d3
Jelte Cost <j.cost@cadicto.nl>
2020-06-02 19:22:37 +0200

    Update applications.js

--- commands/applications.js
+++ commands/applications.js
@@ -8,7 +8,7 @@ module.exports.run = async(bot, message, args) => {
         .setTitle(`Applications:`)
         //.setThumbnail(boticon)
         .setColor("1a4ebd")
-        .setDescription(`Greetings! This application is designed to assess the knowledge, responsibilities, and duties you hold to potentially become a staff member here at British Airways. Here at British Airways, our staff play an integral part in our glorious Airline. Staff play a vital role in the development and enrichment of our Airline, Hence, the maturity, discipline and development of trainees is of paramount importance to our High Ranking Staff. \n \nhttps://www.roblox.com/games/5126107873`);
+        .setDescription(`Greetings! This application is designed to assess the knowledge, responsibilities, and duties you hold to potentially become a staff member here at British Airways. Here at British Airways, our staff play an integral part in our glorious Airline. Staff play a vital role in the development and enrichment of our Airline, Hence, the maturity, discipline and development of trainees is of paramount importance to our High Ranking Staff. \n \n https://docs.google.com/forms/d/1F2Xd18xe1p3XSzjONAV_amKSLZk1o44gp4Pl37CNEqg/edit`);
         
         var embedSend = await message.channel.send(announceEmbed);
         return;
