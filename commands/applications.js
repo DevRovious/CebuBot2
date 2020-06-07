1e34f1b38b4d61fcd62d388aa05231f9dce1eaca
DevRovious <64751936+DevRovious@users.noreply.github.com>
2020-06-07 12:41:53 +0200

    Update applications.js

--- commands/applications.js
+++ commands/applications.js
@@ -6,12 +6,12 @@ module.exports.run = async(client, message, args, boticon) => {
     var icon = message.guild.iconURL;
     // Make an embed
     var announceEmbed = new discord.MessageEmbed()
-        .setTitle(`Britisch Airways Roblox | Announcements`)
+        .setTitle(`Britisch Airways Roblox | Allocation`)
         .setFooter(`Sendend by ${message.author.tag}`)
         .setThumbnail(boticon)
         .setColor("1a4ebd")
         .setTimestamp()
-        .setDescription(` Flight Code: BA 0002 \n Date: \n Staff Join: \n Unslock: \n Airport: \n Aircraft: \n \n \n **Management Roles** \n Host: \n Co-Host: \n Supervisor: \n \n \n **Terminal** \n Check-In/Duty Free: \n \n \n **Flight Deck** \n Captain: \n Backup: \n First Officer: \n Backup: \n \n \n **Cabin Staff** \n Purser (SS+): \n Cabin Crew: \n \n \n **Lounge** \n Gate Desk: \n \n \n **Landside Team** \n Tarmac Supervisor (SS+): \n Landside: `)
+        .setDescription(`\n Flight Code: BA 0002 \n Date: \n Staff Join: \n Unslock: \n Airport: \n Aircraft: \n \n \n **Management Roles** \n Host: \n Co-Host: \n Supervisor: \n \n \n **Terminal** \n Check-In/Duty Free: \n \n \n **Flight Deck** \n Captain: \n Backup: \n First Officer: \n Backup: \n \n \n **Cabin Staff** \n Purser (SS+): \n Cabin Crew: \n \n \n **Lounge** \n Gate Desk: \n \n \n **Landside Team** \n Tarmac Supervisor (SS+): \n Landside: `)
         .setImage('https://cdn.discordapp.com/attachments/690504401017634827/719132286997626960/britishairways_800x350_web.jpg');
         
  
