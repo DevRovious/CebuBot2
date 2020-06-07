506814edcb7c9ed4eff112467edad93f0c433dd0
DevRovious <64751936+DevRovious@users.noreply.github.com>
2020-06-07 15:29:38 +0200

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -1,35 +1,26 @@
-const Discord = require('discord.js')
-const moment = require('moment')
-const ms = require('ms');
+const discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
+module.exports.run = async(client, message, args, boticon) => {
 
-    const duration = moment.duration(client.ontime)  
-
-    let u = convertMS(client.uptime)
-    let ontime = `**${u.h}**` + " Uur" + `**${u.m}**` + " Minuten, " + `**${u.s}**` + " Seconden"
-
-    message.channel.send(`:hammer: | **${message.author.username}**, Ik ben online voor: ${ms(client.uptime)}`)
-
-    function convertMS(ms) {
-        var d, h, m, s;
-        s = Math.floor(ms / 1000);
-        m = Math.floor(s / 60);
-        s = s % 60;
-        h = Math.floor(m / 60);
-        m = m % 60;
-        d = Math.floor(h / 24);
-        h = h % 24;
-
-        return {
-            d: d
-            , h: h
-            , m: m
-            , s: s
-        };
-    };
+    message.delete();
+    var icon = message.guild.iconURL;
+    // Make an embed
+    var announceEmbed = new discord.MessageEmbed()
+        .setTitle(`Britisch Airways Roblox | Allocation`)
+        .setFooter(`Sendend by ${message.author.tag}`)
+        .setThumbnail(boticon)
+        .setColor("1a4ebd")
+        .setTimestamp()
+        .setDescription(`\n Flight Code: BA 0002 \n Date: \n Staff Join: \n Unslock: \n Airport: \n Aircraft: \n \n \n **Management Roles** \n Host: <@476112184372363285> \n Co-Host: \n Supervisor: \n \n \n **Terminal** \n Check-In/Duty Free: \n \n \n **Flight Deck** \n Captain: \n Backup: \n First Officer: \n Backup: \n \n \n **Cabin Staff** \n Purser (SS+): \n Cabin Crew: \n \n \n **Lounge** \n Gate Desk: \n \n \n **Landside Team** \n Tarmac Supervisor (SS+): \n Landside: `)
+        .setImage('');
+        
+ 
+    // Send the embed to the current channel
+    var embedSend = await message.channel.send(announceEmbed);
+    return;
 }
 
+
 module.exports.help = {
-    nome: "uptime"
-}  
+    name: "Flight"
+}
